a072dc4d260bb7c2b6a1687a8825af841161afa9
Vasty Jay <vastyjay@yahoo.com>
2020-12-13 01:37:28 +0000

    change to utils.js

--- cypress/libraryfolder/utils.js
+++ cypress/libraryfolder/utils.js
@@ -16,7 +16,7 @@ export function verifyAlert1(msg) {
   }
 
 export function getRandomNumber() {
-    const min = 1
+    const min = 2
     const max = 100
     return Math.floor(Math.random() * (max - min + 1) + min)//The maximum is inclusive and the minimum is inclusive
 
