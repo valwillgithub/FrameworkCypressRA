11822d2abaced877381183d218409d5c8a67e00e
Vasty Jay <vastyjay@yahoo.com>
2020-12-21 02:08:30 +0000

    cucumber plug-in 2

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -24,3 +24,9 @@ const {downloadFile} = require('cypress-downloadfile/lib/addPlugin')
 module.exports = (on, config) => {
   on('task', {downloadFile})
 }
+
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = (on, config) => {
+  on('file:preprocessor', cucumber())
+}
