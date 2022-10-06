b75d0ab26b9969c05dff0d90b43ddfec23d1c87f
valwillgithub <vastyjay@yahoo.com>
2022-10-06 17:35:08 +0100

    upgrade to cypress v10.9.0

---
+++ cypress.config.js
@@ -0,0 +1,41 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 8000,
+  modifyObstructiveCode: true,
+  experimentalSourceRewriting: true,
+  experimentalWebKitSupport: true,
+  experimentalStudio: true,
+  chromeWebSecurity: false,
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results/specs",
+    overwrite: false,
+    html: false,
+    json: true,
+    charts: true,
+    timestamp: "ddmmyyyy_HHMMss",
+  },
+  env: {
+    url: "http://www.way2automation.com/angularjs-protractor/banking/#/login",
+  },
+  retries: {
+    runMode: 0,
+    openMode: 0,
+  },
+  projectId: "86v319",
+  video: true,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl:
+      "http://www.way2automation.com/angularjs-protractor/banking/#/login",
+    //specPattern: "cypress/integration/**/*.{js,jsx,ts,tsx,feature}",
+    specPattern: "cypress/integration/**/*.feature",
+
+    experimentalSessionAndOrigin: true,
+  },
+});
