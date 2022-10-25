1d59b39737f523450ea688dd05e0185013e55486
valwillgithub <vastyjay@yahoo.com>
2022-10-26 00:52:40 +0100

    Added s3bucket.yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "co3mox",
   defaultCommandTimeout: 8000,
   modifyObstructiveCode: true,
   experimentalSourceRewriting: true,
