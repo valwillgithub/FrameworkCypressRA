9641807ca2f949de4522d137b2014028055b27af
valwillgithub <vastyjay@yahoo.com>
2023-11-26 19:08:02 +0000

    modified cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -36,7 +36,7 @@ module.exports = defineConfig({
 		baseUrl:
 			'http://www.way2automation.com/angularjs-protractor/banking/#/login',
 		//specPattern: "cypress/integration/**/*.{js,jsx,ts,tsx,feature}",
-		//specPattern: "cypress/integration/**/*.feature",
+		specPattern: 'cypress/integration/**/*.feature',
 
 		experimentalSessionAndOrigin: true,
 	},
